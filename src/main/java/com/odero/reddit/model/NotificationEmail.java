3180faa628ae95903811c28cd57dbb843a489f3c
OderoOluoch <oderoluoch@gmail.com>
2021-08-01 13:10:56 +0300

    All domain models and entities set up.

--- src/main/java/com/odero/reddit/model/NotificationEmail.java
+++ src/main/java/com/odero/reddit/model/NotificationEmail.java
@@ -1,4 +1,15 @@
 package com.odero.reddit.model;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NotificationEmail {
+    private String subject;
+    private String recipient;
+    private String body;
 }
